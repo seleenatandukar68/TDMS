496b69922273904ab6289b602cfa3731c37bdb74
Seleena Tandukar <seleena68@gmail.com>
2021-04-29 15:52:57 +1000

    routing setup success

--- TDMS/client-app/src/App.vue
+++ TDMS/client-app/src/App.vue
@@ -1,14 +1,23 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+       <TopMenu/>
+      
     </div>
     <router-view />
+   
   </div>
 </template>
-
-<style lang="scss">
+<script>
+import TopMenu from "@/components/Menu/TopMenu.vue";
+export default {
+  name :'app',
+  components:{
+    TopMenu
+  }
+}
+</script>
+<style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -19,14 +28,14 @@
 
 #nav {
   padding: 30px;
+}
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
 
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+#nav a.router-link-exact-active {
+  color: #42b983;
 }
 </style>
--- TDMS/client-app/src/vue.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    // The URL where the .NET Core app will be listening.
-    // Specific port depends on whether IISExpress/Kestrel and HTTP/HTTPS are used
-    devServer: {
-      proxy: 'http://localhost:5000'
-    },
-  }
